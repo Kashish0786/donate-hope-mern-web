9f34e85752d9ed150f21c1363722652af216175b
Kashish0786 <siddiquekashish654@gmail.com>
2026-04-28 18:12:35 +0530

    fix: update donation routes and rename authMiddleware

--- backend/routes/donationRoutes.js
+++ backend/routes/donationRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createDonation, getMyDonations } from "../controllers/donationCont.js";
-import { protect } from "../middleware/authmiddleware.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.post("/", protect, createDonation);
